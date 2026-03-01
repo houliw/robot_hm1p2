74394cd94d6764f165c4bf8ae94c382a9e8ba443
houliw <houliw@163.com>
2026-03-01 22:28:06 +0800

    function play

--- robot_test.py
+++ robot_test.py
@@ -5,15 +5,11 @@ import matplotlib.pyplot as plt
 from matplotlib.patches import Circle as CirclePatch
 
 robot_name = "monkey"
-battery_level = 100.0
+
 position = np.array([0.0, 0.0])
 velocity = np.array([1.0, 1.5])
 obstacle_pos = np.array([4.5, 4.5])
 obstacle_size = 1.0
-
-test = position + velocity
-print(test)
-is_running = True
 
 def move(current_position, speed):
     noise = [random.uniform(-0.1, 0.1),random.uniform(-0.1, 0.1)]
@@ -32,40 +28,51 @@ def obstacle(robot_pos, obs_pos,obs_size):
 
 print(f"[{robot_name}] System Start...")
 
-fig = plt.figure()
-plt.ion()
 
+def play(position, velocity, obstacle_pos, obstacle_size):
+    is_running = True
+    battery_level = 100.0
 
+    fig = plt.figure()
+    plt.ion()
 
+    while is_running:
+        if battery_level < 20:
+            print("Warning: Low Battery!")
+            is_running = False
+            break
 
-while is_running:
-    if battery_level < 20:
-        print("Warning: Low Battery!")
-        is_running = False
-        break
+        position = move(position, velocity)
+        if obstacle(position, obstacle_pos, obstacle_size):
+            print("Warning: Obstacle!")
+            is_running = False
+            break
 
-    position = move(position, velocity)
-    if obstacle(position, obstacle_pos, obstacle_size):
-        print("Warning: Obstacle!")
-        is_running = False
-        break
+        battery_level -= 5.0
 
-    battery_level -= 5.0
+        print(f"Status: Moving | Pos: [{position[0]:5.2f}m, {position[1]:5.2f}m | Battery Level: {battery_level}%" )
+        ax = fig.add_subplot(111)
+        ax.set_xlim(0, 30)
+        ax.set_ylim(0, 30)
+        ax.set_xlabel('X')
+        ax.set_ylabel('Y')
 
-    print(f"Status: Moving | Pos: [{position[0]:5.2f}m, {position[1]:5.2f}m | Battery Level: {battery_level}%" )
-    ax = fig.add_subplot(111)
-    ax.set_xlim(0, 30)
-    ax.set_ylim(0, 30)
-    ax.set_xlabel('X')
-    ax.set_ylabel('Y')
+        ax.plot(position[0], position[1], 'ro')
+        draw_circle = plt.Circle((obstacle_pos[0],obstacle_pos[1]), radius=obstacle_size)
+        ax.set_aspect(1)
+        ax.add_artist(draw_circle)
+        plt.pause(0.5)
+     #   time.sleep(0.5)
 
-    ax.plot(position[0], position[1], 'ro')
-    draw_circle = plt.Circle((obstacle_pos[0],obstacle_pos[1]), radius=obstacle_size)
-    ax.set_aspect(1)
-    ax.add_artist(draw_circle)
-    plt.pause(0.5)
- #   time.sleep(0.5)
+    plt.ioff()
+    plt.show()
 
-plt.ioff()
-plt.show()
+play(position, velocity, obstacle_pos, obstacle_size)
+
+position = np.array([0.0, 0.0])
+velocity = np.array([1.0, 1.5])
+obstacle_pos = np.array([3.0, 4.5])
+obstacle_size = 1.0
+play(position, velocity, obstacle_pos, obstacle_size)
+
 print("System Shutdown.")
